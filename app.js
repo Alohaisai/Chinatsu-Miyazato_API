fabb086b99dac1f67ae04a3e03f04748cab2fe65
thousand7274 <thousand7274@gmail.com>
2019-08-15 15:18:50 +0900

    js,index修正
    javascriptのコードとindexのtableのidを修正しました

--- app.js
+++ app.js
@@ -1,34 +1,11 @@
 
-$( function (){
-  let url = 'https://api.gnavi.co.jp/RestSearchAPI/v3/'
-  let param = {
-    keyid:'ca4a4dd4f35d46a67dee1144cdaf0292',
-    range:'3',
-    latitude:'26.316166',
-    longitude:'127.757899'
-    }
-  
-    $.getJSON(
-      url, //リクエストURL
-      param,       //送信データ
-      function(data, status) {
-        console.log(data);
-        for(let item of data.rest){
-          $('#list').append('<li>'+ item.name + '</li>')
-      }
-          $('input[name=search]').on('keyup', function(){
-          $('#list').append('<li>'+ name + '</li>')
-        })
-      }
-    );
-  });
-  
+
   function showResult( result ) {
     for ( let i in result.rest ) {
       $("#table").append("<tr><td>" + result.rest[ i ].name + "</td><td>" + result.rest[ i ].opentime )
     }
   }
-  
+
   $( function () {
     let url = 'https://api.gnavi.co.jp/RestSearchAPI/v3/'
     let param = {
@@ -36,16 +13,14 @@ $( function (){
       range:'3',
       latitude:'26.316166',
       longitude:'127.757899',
+      // hit_per_page: '100',
       freeword:''
   }
-  
+
     $("#submit").on("click", function () {
-      param.freeword = $("category").val()
+      param.freeword = $("#search").val()
       $.getJSON( url, param, function( result ) {
         showResult( result )
       });
     });
   });
-  
-  
-  
