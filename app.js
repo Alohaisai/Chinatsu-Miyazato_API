cd40a44e54e12d653b47d9ded9c4396ef02e5982
thousand7274 <thousand7274@gmail.com>
2019-08-15 17:04:22 +0900

    javascriptの修正、indexを少し修正
    javascriptのコードを修正、indexを少し修正しました

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 $( function () {
+  // let img = result.rest[i].image_url.shop_image1
+  // let name = result.rest[i].name
+  // let time = result.rest[i].opentime
+  // let address = result.rest[i].address
+  // let tel = result.rest[i].tel
+  // let site = result.rest[i].url
   function showResult( result ) {
     for ( let i in result.rest ) {
-      $("#table").append("<tr><td>" + "<img src= "+ result.rest[i].image_url.shop_image1 + " >" + "<tr><td>"+ result.rest[ i ].name + "</td><td>" + result.rest[ i ].opentime + "</td><td>" + result.rest[i].address + "</td><td>" + result.rest[i].tel + "</td><td>" + result.rest[i].url)
-      // $(".card-title").append('result.rest[ i ].name');
-
+      let img = "<div class= card><div class= body><div class= card-img><img src= "+ result.rest[i].image_url.shop_image1 + " ></div>"
+      let name = "<h5 class= card-title>" + result.rest[i].name + "</h5>"
+      let time = "<li>" + result.rest[i].opentime + "</li>"
+      let address = "<li>" + result.rest[i].address + "</li>"
+      let tel = "<li>" + result.rest[i].tel + "</li>"
+      // $("#table").append("<tr><td>" + "<img src= "+ result.rest[i].image_url.shop_image1 + " >" + "<tr><td>"+ result.rest[ i ].name + "</td><td>" + result.rest[ i ].opentime + "</td><td>" + result.rest[i].address + "</td><td>" + result.rest[i].tel + "</td><td>" + result.rest[i].url)
+      $(".search-result").before(img);
+      $(".card-body").append(name);
+      $(".card-body").after("<ul>" + time + address + tel +"</ul></div></div>")
+      
 
     }
   }
