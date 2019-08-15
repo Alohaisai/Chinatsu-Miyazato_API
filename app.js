3a8fb8d445fd78ebfa415a0f90e578f8e5832f46
alohaisai <mm70aloha@gmail.com>
2019-08-16 00:07:18 +0900

    画像、js、css、htmlを追加
    とりあえず形にしてみました

--- app.js
+++ app.js
@@ -1,51 +1,19 @@
-
-$( function (){
-  let url = 'https://api.gnavi.co.jp/RestSearchAPI/v3/'
-  let param = {
-    keyid:'ca4a4dd4f35d46a67dee1144cdaf0292',
-    range:'3',
-    latitude:'26.316166',
-    longitude:'127.757899'
-    }
-  
-    // $.getJSON(
-    //   url, //リクエストURL
-    //   param,       //送信データ
-    //   function(data, status) {
-    //     console.log(data);
-    //     for(let item of data.rest){
-    //       $('#list').append('<li>'+ item.name + '</li>')
-    //   }
-    //       $('input[name=search]').on('keyup', function(){
-    //       $('#list').append('<li>'+ name + '</li>')
-    //     })
-    //   }
-    // );
+$(function(){
+  // スムーズスクロール トップに戻る
+  $('.js-scroll').on('click',function(){
+    $('body,html').animate({scrollTop: 0},500);
+    return false;
   });
   
-  function showResult( result ) {
-    for ( let i in result.rest ) {
-      $("#table").append("<tr><td>" + result.rest[ i ].name + "</td><td>" + result.rest[ i ].opentime )
-    }
-  }
-  
-  $( function () {
-    let url = 'https://api.gnavi.co.jp/RestSearchAPI/v3/'
-    let param = {
-      keyid:'ca4a4dd4f35d46a67dee1144cdaf0292',
-      range:'3',
-      latitude:'26.316166',
-      longitude:'127.757899',
-      freeword:''
-  }
-
-    $("#submit").on("click", function () {
-      param.freeword = $("category").val()
-      $.getJSON( url, param, function( result ) {
-        showResult( result )
-      });
-    });
+  // 検索ボタンでfoundまでスクロール
+  $('.submit').on('click',function(){
+    $('.container').slideDown(1000);
+    $("html,body").animate({scrollTop:$('.found').offset().top}, 2000);
+    return false;
   });
-
   
+  // $('.submit').on('click',function(){
+  //   $('.container').slideDown(1000);
+  // });
   
+  });
