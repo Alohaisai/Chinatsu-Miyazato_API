3087680eb7a1f69a9d57ad797537112f5b76cd67
thousand7274 <thousand7274@gmail.com>
2019-08-16 15:17:20 +0900

    noimage追加
    もしお店の画像がない場合noimage画像を追加されるようにしました

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ $( function () {
 
   function showResult( result ) {
     
-
     for ( let i in result.rest) {
 
-      // let imageTag = result.rest[i].image_url.shop_image1
+      let src = '';
+      if(result.rest[i].image_url.shop_image1 !== ''){
+        console.log(result.rest[i].image_url.shop_image1);
+        src = result.rest[i].image_url.shop_image1;
+      }else{
+        src = "noimage.jpg";
+      }
 
+      let resultCard ='<div class= col-lg-4 ><div class= card><div class= card-body ><div class= text-center ><img src=' + src + '></div><div class= text-center> <div class=mt-3><h5 class= card-title>' + result.rest[i].name + '</h5></div></div>'+'<ul class= pl-0><li class= list-group>' + result.rest[i].opentime + '</li><li class= list-group>' + result.rest[i].address + '</li><li class= list-group>' + result.rest[i].tel + '</li></ul></div></div></div></div>'
 
-      let resultCard ='<div class= col-lg-4 ><div class= card><div class= card-body ><div class= text-center ><img src=' + result.rest[i].image_url.shop_image1 + '></div><div class= text-center> <div class=mt-3><h5 class= card-title>' + result.rest[i].name + '</h5></div></div>'+'<ul class= pl-0><li class= list-group>' + result.rest[i].opentime + '</li><li class= list-group>' + result.rest[i].address + '</li><li class= list-group>' + result.rest[i].tel + '</li></ul></div></div></div></div>'
-
+      $('img').attr('class','shop-img')
       $('.row').prepend(resultCard);
 
     }
@@ -28,6 +33,8 @@ $( function () {
 
   $('#submit').on('click', function () {
     
+    $('.row').empty()
+
     param.freeword = $('#search').val()
     $.getJSON( url, param, function( result ) {
       showResult( result )
